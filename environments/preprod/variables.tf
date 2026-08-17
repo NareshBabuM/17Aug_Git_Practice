5c169eea677ee8d9cbb8c0b01ebda36f333869a2
Naresh <naresh.babu.medehal@gmail.com>
2026-08-17 22:50:55 +0530

    simple code

--- environments/preprod/variables.tf
+++ environments/preprod/variables.tf
@@ -9,27 +9,3 @@ variable "vnets" {
 variable "subnets" {
   type = map(any)
 }
-
-variable "public_ips" {
-  type = map(any)
-}
-
-variable "vms" {
-  type = map(any)
-}
-
-variable "bastions" {
-  type = map(any)
-}
-
-variable "app_gateways" {
-  type = map(any)
-}
-
-variable "load_balancers" {
-  type = map(any)
-}
-
-variable "key_vaults" {
-  type = map(any)
-}
